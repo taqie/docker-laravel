36b5c0c1a283a9f72fa78589af1fbe19548dcb64
Gracjan Kubicki <gracjan@directinfo.pl>
2021-03-19 00:23:55 +0100

    master, update entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/etc/init.d/ssh start && /etc/init.d/cron start && /etc/init.d/php8.0-fpm start && /usr/bin/supervisord -c /etc/supervisor/supervisord.conf && nginx -g "daemon off;"
+set -e
+
+php artisan migrate && php artisan config:cache && php artisan route:cache && php artisan view:cache && /etc/init.d/ssh start && /etc/init.d/cron start && /etc/init.d/php8.0-fpm start && /usr/bin/supervisord -c /etc/supervisor/supervisord.conf && nginx -g "daemon off;"
+
+exec "$@"
